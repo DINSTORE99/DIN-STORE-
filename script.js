db16235580b7657f5de301249a75e55b3ca4ecba
DINSTORE99 <dinstoree41@gmail.com>
2025-11-07 18:19:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -442,9 +442,11 @@ renderCategory('🖥VPS Digital Ocean', vpsProducts);
 
 const sectionBtns=document.createElement('section');
 sectionBtns.innerHTML=`
-  <div style="margin:20px 0;text-align:center;display:flex;gap:10px;justify-content:center;flex-wrap:wrap">
-    <a href="dinhc.vercel.app"target="_blank" class="shiny" style="flex:1;min-width:140px;max-width:200px;background:linear-gradient(90deg,#2563eb,#3b82f6);padding:12px;border-radius:12px;color:#fff;font-weight:600;text-decoration:none;display:flex;align-items:center;justify-content:center;gap:8px">
-      <i class="fas fa-sync-alt"></i> Update Config
+  <div style="margin:20px 0;text-align:center;display:flex;gap:10px;justify-content:center;flex-wrap:wrap;">
+  <a href="https://dinhc.vercel.app" target="_blank" class="shiny" 
+     style="flex:1;min-width:140px;max-width:200px;background:linear-gradient(90deg,#2563eb,#3b82f6);padding:12px;border-radius:12px;color:#fff;font-weight:600;text-decoration:none;display:flex;align-items:center;justify-content:center;gap:8px;">
+    <i class="fas fa-sync-alt"></i> Update Config
+
     </a>
     <a href="https://whatsapp.com/channel/0029Vaz4J9rHLHQaCsN2NN0O" target="_blank" class="shiny" style="flex:1;min-width:140px;max-width:200px;background:linear-gradient(90deg,#2563eb,#3b82f6);padding:12px;border-radius:12px;color:#fff;font-weight:600;text-decoration:none;display:flex;align-items:center;justify-content:center;gap:8px">
       <i class="fas fa-bullhorn"></i> Saluran
